e67da9e9fc2fd37567e05a6410ed967e7c8927f4
Foolish-Genius <saxena27bharat@gmail.com>
2025-09-16 20:54:59 +0530

    small update

--- src/components/SectionList.jsx
+++ src/components/SectionList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import checkUrls from '../utils/checkURLs';
+import checkUrls from '../utils/checkURLs.js';
 import RedirectTimer from './RedirectTimer';
 
 export default function SectionList() {
